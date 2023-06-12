a683b7b3ef43c141daecb82e459886c5c33554c3
echo <infidelLOLer@gmail.com>
2023-06-12 22:41:36 +0330

    advanced gamering
    im literally in love with ducko

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as Gluon from '@gluon-framework/gluon';
+import amongusballsdickcockasspussyboobs from './injection.js'
+
+const Window = await Gluon.open('https://open.spotify.com');
+
+await new Promise((resolve) => {
+  setTimeout(resolve, 1000);
+});
+console.log("shit")
+await Window.resources.js(amongusballsdickcockasspussyboobs());
