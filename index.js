efa4039ae5f99a8105effb9c757525ff6695f058
exhq <infidelLOLer@gmail.com>
2023-06-13 13:23:17 +0330

    i HAVE to show off my creation

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import * as Gluon from '@gluon-framework/gluon';
-import amongusballsdickcockasspussyboobs from './injection.js'
+import injection from './injection.js';
 
 const Window = await Gluon.open('https://open.spotify.com');
 
 await new Promise((resolve) => {
   setTimeout(resolve, 1000);
 });
-console.log("shit")
-await Window.resources.js(amongusballsdickcockasspussyboobs());
+
+
+
+await Window.resources.js(injection.toString().slice(21, -1));
--- injection.js
+++ injection.js
@@ -1,4 +1,10 @@
-export default function amongusballsdickcockasspussyboobs(){  
+export default function injection(){  
+    setInterval(() => {
+        document.getElementsByClassName("Type__TypeElement-sc-goli3j-0 bkjCej")[0].innerText = "Echotify";
+      }, 200);
 
-    return ``
+
+      
 }
+
+
