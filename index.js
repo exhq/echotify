e0fd09027423f9872bbb19e83419c72cf3bc8362
exhq <infidelLOLer@gmail.com>
2023-06-13 15:13:33 +0330

    :trollface:

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import { readFile } from 'fs';
 
 const Window = await Gluon.open('https://open.spotify.com');
 
-
-const filePath = './injection.js';
+const filePaths = ['./injection.js', './injection.css'];
 
 const readAndProcessFile = () => {
-  readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    Window.resources.js(data)
+  filePaths.forEach(filePath => {
+    readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (filePath === './injection.js') {
+        Window.resources.js(data);
+      } else if (filePath === './injection.css') {
+        Window.resources.css(data);
+      }
+    });
   });
 };
 
 setInterval(readAndProcessFile, 200);
+
 
 
--- injection.js
+++ injection.js
@@ -1,3 +1,4 @@
+//home button
 document.getElementsByClassName("Type__TypeElement-sc-goli3j-0 bkjCej")[0].innerText = "echotify";
 
 //disabling the stupid ctrl + shift + o bullshittery
